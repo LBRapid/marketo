c504aeeeea127a4d1d6efea417767466a4d60200
John Dyer <lbrapid@gmail.com>
2013-10-09 11:03:37 -0400

    Supress Savon logging

--- lib/marketo.rb
+++ lib/marketo.rb
@@ -1,7 +1,7 @@
 require 'savon'
 
 Savon.configure do |config|
-  # config.log = true            # disable logging
+  config.log = false            # disable logging
   # config.log_level = :info      # changing the log level
   # config.logger = Rails.logger  # using the Rails logger
   config.pretty_print_xml = true
